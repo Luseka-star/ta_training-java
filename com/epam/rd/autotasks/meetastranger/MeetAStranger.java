ad61441e0d237535546027d8bfcfcdf549ada61b
Liudmyla Stambula <liudmylastambula@gmail.com>
2023-04-27 12:18:09 +0200

    PAssed

---
+++ com/epam/rd/autotasks/meetastranger/MeetAStranger.java
@@ -0,0 +1,12 @@
+package com.epam.rd.autotasks.meetastranger;
+
+import java.util.Scanner;
+
+public class MeetAStranger {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.next();
+
+        System.out.println("Hello, " + input);
+    }
+}
